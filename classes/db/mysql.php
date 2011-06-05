65236ce4fcf1d531b21d539acff67dbcc01066b3
Mark Findlay <mark.eff1@hotmail.co.uk>
2011-06-05 15:17:28 +0100

    Creating simple file structure.

---
+++ classes/db/mysql.php
@@ -0,0 +1,48 @@
+<?PHP
+/**
+Simple MySQL Class currently under development.
+
+Ideally a custom exception class should be created.
+*/
+
+class Mysql {
+
+	protected $username;
+	protected $password;
+	protected $host;
+	
+	public function __construct($user, $pass, $host) {
+		$this->username = $user;
+		$this->password = $pass;
+		$this->host		= $host;
+	}
+	
+	public function connect() {
+		if (!$connection = mysql_connect($this->host, $this->username, $this->password)) {
+			throw new Exception("Exception: " . mysql_error());
+		}
+		return $connection;
+	}
+	
+	public function selectDB($connection, $tableName) {
+		if (!$connection) {
+			throw new Exception("Exception: Empty parameter given ");
+		}
+		if (!$selectedDB = mysql_select_db()) {
+			throw new Exception ("Exception: Could not select database" . mysql_error());
+		}
+		return $selectedDB;
+	}
+	
+	public function query($sql, $connection = NULL) {
+		if (!$sql) {
+			throw new Exception ("Exception: No query string given");
+		}
+		if (!$result = mysql_query($sql)) {
+		
+		}
+	}
+
+}
+
+?>
