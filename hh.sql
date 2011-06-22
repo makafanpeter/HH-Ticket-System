885fa1443271e216b6bf2df2ae0f99dc22281a89
bigalan09 <bigalan09@gmail.com>
2011-06-22 23:41:06 +0100

    Edited ticket table.

--- hh.sql
+++ hh.sql
@@ -87,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `ticket` (
   `title` varchar(150) NOT NULL,
   `message` text NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
-  `creator` int(6) NOT NULL,
+  `user_id` int(6) NOT NULL,
   `created_date` int(20) NOT NULL,
   PRIMARY KEY (`ticket_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
